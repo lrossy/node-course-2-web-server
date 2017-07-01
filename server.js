694507a5a2dce5f920e16d3195db1b3409680b7e
Luke <lrossy@gmail.com>
2017-07-01 14:42:55 -0400

    fixed heroku deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-var port = (process.env.port)? process.env.port: 3000;
+const port = (process.env.PORT)? process.env.PORT: 3000;
 
 var app = express();
 
